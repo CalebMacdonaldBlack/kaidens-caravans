7f2b4f82468b62e801a8ac834862afc5e836f4ea
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-16 00:03:12 +1000

    created test for handlers

--- src/cljs/kaidens_caravans/handlers.cljs
+++ src/cljs/kaidens_caravans/handlers.cljs
@@ -5,45 +5,17 @@
             [re-frame.core :refer [dispatch reg-event-db reg-event-fx]]
             [ajax.core :as ajax]))
 
-(reg-event-db
-  :initialize-db
-  (fn [_ _]
-    db/default-db))
+(defn- initialize-db [_ _] db/default-db)
+(reg-event-db :initialize-db initialize-db)
 
-(reg-event-db
-  :set-active-page
-  (fn [db [_ page]]
-    (assoc db :page page)))
+(defn- set-active-page [db [_ page]] (assoc db :page page))
+(reg-event-db :set-active-page set-active-page)
 
-(reg-event-fx
-  :clear-current-caravan
-  (fn [{:keys [db]} _]
-    (reset! (:current-caravan db) {})
-    {}))
+(defn- clear-current-caravan [{:keys [db]} _] (reset! (:current-caravan db) {}) {})
+(reg-event-fx :clear-current-caravan clear-current-caravan)
 
-(reg-event-fx
-  ::success-create-caravan
-  (fn [_ [_ result]]
-    (cljs.pprint/pprint result)))
-
-(reg-event-fx
-  ::failure-create-caravan
-  (fn [_ [_ result]]
-    (cljs.pprint/pprint result)))
-
-(reg-event-db
-  :good-post-result
-  (fn [db [_ result]]
-    (assoc db :api-result result)))
-
-(reg-event-db
-  :bad-post-result
-  (fn [db [_ result]]
-    (assoc db :api-result result)))
-
-(reg-event-fx
-  :create-caravan
-  (fn [_ [_ caravan]]
-    (post-json {:url "/caravans"
-                :body caravan
-                :after-success (rf/dispatch [:clear-current-caravan])})))
+(defn- create-caravan [_ [_ caravan]]
+  (post-json {:url "/caravans"
+              :body caravan
+              :after-success (rf/dispatch [:clear-current-caravan])}))
+(reg-event-fx :create-caravan create-caravan)
---
+++ test/cljs/kaidens_caravans/test/handler.cljs
@@ -0,0 +1,29 @@
+(ns kaidens-caravans.test.handler
+  (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
+            [kaidens-caravans.ajax :refer [post-json]]
+            [pjstadig.humane-test-output]
+            [reagent.core :as r]
+            [kaidens-caravans.handlers :as h]))
+
+(deftest handlers
+
+  (testing "initialize-db"
+    (let [initialized-db kaidens-caravans.db/default-db
+          initialize-db #'h/initialize-db]
+      (is (= initialized-db (initialize-db nil nil)))))
+
+  (testing "set-active-page"
+    (let [set-active-page #'h/set-active-page]
+      (is (= {:page :home} (set-active-page {} [nil :home])))))
+
+  (testing "clear-current-caravan"
+    (let [clear-current-caravan #'h/clear-current-caravan
+          current-caravan (r/atom {:test "test"})
+          cofx {:db {:current-caravan current-caravan}}]
+      (is (= {} (clear-current-caravan cofx nil)))
+      (is (= {} @current-caravan))))
+
+  (testing "create-caravan"
+    (with-redefs [post-json (constantly "called")]
+      (let [create-caravan #'h/create-caravan]
+        (is (= "called" (create-caravan {:test "hello"})))))))
