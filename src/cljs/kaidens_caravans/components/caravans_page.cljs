55787da7659282d12d17a56a206cdd6aef7692bd
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-15 23:12:46 +1000

    removed unused param

--- src/cljs/kaidens_caravans/components/caravans_page.cljs
+++ src/cljs/kaidens_caravans/components/caravans_page.cljs
@@ -7,7 +7,7 @@
 (secretary/defroute "/caravans" []
                     (rf/dispatch [:set-active-page :caravans]))
 
-(defn form-input [name key size type ratom]
+(defn form-input [name key type ratom]
   [:div.form-group.row
    [:label.col-2.col-form-label name]
    [:div.col-10>input.form-control {:type type
@@ -16,21 +16,21 @@
 
 (defn modify-caravan-form [caravan]
   [:div
-   [form-input "Make" :make 3 "text" caravan]
-   [form-input "Model" :model 3 "text" caravan]
-   [form-input "Type" :type 2 "text" caravan]
-   [form-input "VIN Number" :vin 2 "text" caravan]
-   [form-input "Frame" :frame 2 "text" caravan]
-   [form-input "Terrain" :terrain 2 "text" caravan]
-   [form-input "Length (feet)" :feet 2 "number" caravan]
-   [form-input "Weight (Tonne)" :tonne 2 "number" caravan]
-   [form-input "Year" :year 2 "number" caravan]
-   [form-input "Axles" :axles 2 "text" caravan]
-   [form-input "Condition" :condition 2 "text" caravan]
-   [form-input "Bed" :bed 2 "text" caravan]
-   [form-input "Fridge" :fridge 2 "text" caravan]
-   [form-input "Suspension" :suspension 2 "text" caravan]
-   [form-input "price" :price 2 "number" caravan]])
+   [form-input "Make" :make"text" caravan]
+   [form-input "Model" :model"text" caravan]
+   [form-input "Type" :type"text" caravan]
+   [form-input "VIN Number" :vin "text" caravan]
+   [form-input "Frame" :frame "text" caravan]
+   [form-input "Terrain" :terrain "text" caravan]
+   [form-input "Length (feet)" :feet "number" caravan]
+   [form-input "Weight (Tonne)" :tonne "number" caravan]
+   [form-input "Year" :year "number" caravan]
+   [form-input "Axles" :axles "text" caravan]
+   [form-input "Condition" :condition "text" caravan]
+   [form-input "Bed" :bed "text" caravan]
+   [form-input "Fridge" :fridge "text" caravan]
+   [form-input "Suspension" :suspension "text" caravan]
+   [form-input "price" :price "number" caravan]])
 
 (defn caravans-page []
   (let [caravan @(rf/subscribe [:current-caravan])]
