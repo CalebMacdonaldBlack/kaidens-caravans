04ee04693791134243e53c8ca6875bba8b1c8804
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-15 23:09:18 +1000

    navbar menu now works

--- src/cljs/kaidens_caravans/components/nav.cljs
+++ src/cljs/kaidens_caravans/components/nav.cljs
@@ -4,10 +4,15 @@
 
 (defn nav []
   [:nav.navbar.navbar-toggleable-md.navbar-light.bg-faded
-   [:button.navbar-toggler.navbar-toggler-right {:type "button" :data-toggle "collapse"}
+   [:button.navbar-toggler.navbar-toggler-right {:type "button"
+                                                 :data-toggle "collapse"
+                                                 :data-target "#navbarNavAltMarkup"
+                                                 :aria-controls "navbarNavAltMarkup"
+                                                 :aria-expanded "false"
+                                                 :aria-label "Toggle navigation"}
     [:span.navbar-toggler-icon]]
    [:a.navbar-brand {:on-click #(accountant/navigate! "#/")} "Manager"]
-   [:div#navbarNav.collapse.navbar-collapse
+   [:div#navbarNavAltMarkup.collapse.navbar-collapse
     [:ul.navbar-nav
      [:li.nav-item
       [:a.nav-link {:on-click #(accountant/navigate! "#/")} "Home"]]
