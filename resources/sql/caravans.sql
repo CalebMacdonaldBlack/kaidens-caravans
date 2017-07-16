12f11d4028a7be345d14a37880b136cca2593384
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-16 20:32:56 +1000

    basic filtering

--- resources/sql/caravans.sql
+++ resources/sql/caravans.sql
@@ -22,16 +22,16 @@ INSERT INTO caravans
  archived)
 VALUES
   (:type,
-   :make,
-   :model,
-   :price::DOUBLE PRECISION,
-   :year::SMALLINT,
-   :feet::SMALLINT,
-   :tonne::DOUBLE PRECISION,
-   :features,
-   :photos,
-   :videos,
-   :axles,
+    :make,
+    :model,
+    :price :: DOUBLE PRECISION,
+    :year :: SMALLINT,
+    :feet :: SMALLINT,
+    :tonne :: DOUBLE PRECISION,
+    :features,
+    :photos,
+    :videos,
+    :axles,
    :terrain,
    :bed,
    :fridge,
@@ -39,40 +39,56 @@ VALUES
    :suspension,
    :condition,
    :vin,
-   :archived::BOOLEAN);
+   :archived :: BOOLEAN);
 
 -- :name retrieve-caravans :? :*
 -- :doc retrieves caravans
 SELECT *
-FROM caravans;
+FROM caravans
+WHERE COALESCE(type, '') ILIKE /*~ (str "'%" (:type params) "%'") ~*/ AND
+      COALESCE(make, '') ILIKE /*~ (str "'%" (:make params) "%'") ~*/ AND
+      COALESCE(model, '') ILIKE /*~ (str "'%" (:model params) "%'") ~*/ AND
+      COALESCE(price :: VARCHAR, '') LIKE /*~ (str "'%" (:price params) "%'") ~*/ AND
+      COALESCE(year :: VARCHAR, '') LIKE /*~ (str "'%" (:year params) "%'") ~*/ AND
+      COALESCE(feet :: VARCHAR, '') LIKE /*~ (str "'%" (:feet params) "%'") ~*/ AND
+      COALESCE(tonne :: VARCHAR, '') LIKE /*~ (str "'%" (:tonne params) "%'") ~*/ AND
+      COALESCE(axles :: VARCHAR, '') LIKE /*~ (str "'%" (:axles params) "%'") ~*/ AND
+      COALESCE(terrain, '') ILIKE /*~ (str "'%" (:terrain params) "%'") ~*/ AND
+      COALESCE(bed, '') ILIKE /*~ (str "'%" (:bed params) "%'") ~*/ AND
+      COALESCE(fridge, '') ILIKE /*~ (str "'%" (:fridge params) "%'") ~*/ AND
+      COALESCE(frame, '') ILIKE /*~ (str "'%" (:frame params) "%'") ~*/ AND
+      COALESCE(suspension, '') ILIKE /*~ (str "'%" (:suspension params) "%'") ~*/ AND
+      COALESCE(condition, '') ILIKE /*~ (str "'%" (:condition params) "%'") ~*/ AND
+      COALESCE(vin, '') ILIKE /*~ (str "'%" (:vin params) "%'") ~*/ AND
+      COALESCE(archived :: VARCHAR, '') ILIKE /*~ (str "'%" (:archived params) "%'") ~*/;
 
 -- :name update-caravan! :! :n
 -- :doc update an existing caravan record give the id
 UPDATE caravans
 SET
-  type = :type,
-  make = :make,
-  model = :model,
-  price = :price::DOUBLE PRECISION,
-  year = :year::SMALLINT,
-  feet = :feet::SMALLINT,
-  tonne = :tonne::DOUBLE PRECISION,
-  features = :features,
-  photos = :photos,
-  videos = :videos,
-  axles = :axles,
-  terrain = :terrain,
-  bed = :bed,
-  fridge = :fridge,
-  frame = :frame,
+  type       = :type,
+  make       = :make,
+  model      = :model,
+  price      = :price :: DOUBLE PRECISION,
+  YEAR       = :YEAR :: SMALLINT,
+  feet       = :feet :: SMALLINT,
+  tonne      = :tonne :: DOUBLE PRECISION,
+  features   = :features,
+  photos     = :photos,
+  videos     = :videos,
+  axles      = :axles,
+  terrain    = :terrain,
+  bed        = :bed,
+  fridge     = :fridge,
+  frame      = :frame,
   suspension = :suspension,
-  condition = :condition,
-  vin = :vin,
-  archived = :archived::BOOLEAN
-WHERE id = :id::UUID;
+  condition  = :condition,
+  vin        = :vin,
+  archived   = :archived :: BOOLEAN
+WHERE id = :id :: UUID;
 
 -- :name delete-caravan! :! :n
 -- :doc delete a caravan record given the id
 UPDATE caravans
 SET archived = TRUE
-WHERE id = :id::UUID;
+WHERE id = :id :: UUID;
