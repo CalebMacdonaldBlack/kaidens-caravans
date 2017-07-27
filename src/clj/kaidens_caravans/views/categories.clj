144197d1cbff24b1aa347de18da6b54704b3f74d
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-27 23:07:11 +1000

    adjusted webpage categories

--- src/clj/kaidens_caravans/views/categories.clj
+++ src/clj/kaidens_caravans/views/categories.clj
@@ -31,24 +31,7 @@
       [:div.card.lift-card
        {:style "min-height: 330px; max-height: 330px;"}
        [:img.card-img-top
-        {:style "width: 70%; margin: auto; margin-top: 2em",
+        {:style "width: 90%; margin: auto; margin-top: 2.8em; margin-bottom: 1.2em;",
          :alt "Card image cap",
-         :src "/img/camper-trailer.png"}]
-       [:div.card-block [:h4.card-title "Camper Trailer"]]]]]
-    [:div.row.mt-5
-     [:div.col-md-4.offset-md-2
-      [:div.card.lift-card
-       {:style "min-height: 330px; max-height: 330px;"}
-       [:img.card-img-top.style_prevu_kit
-        {:style "width: 70%; margin: auto; margin-top: 4em",
-         :alt "Card image cap",
-         :src "/img/motorhome.png"}]
-       [:div.card-block [:h4.card-title "Motorhome"]]]]
-     [:div.col-md-4
-      [:div.card.lift-card
-       {:style "min-height: 330px; max-height: 330px;"}
-       [:img.card-img-top
-        {:style "width: 80%; margin: auto;",
-         :alt "Card image cap",
-         :src "/img/boat.png"}]
-       [:div.card-block [:h4.card-title "Boat"]]]]]]])
+         :src "/img/other.png"}]
+       [:div.card-block [:h4.card-title "Other"]]]]]]])
