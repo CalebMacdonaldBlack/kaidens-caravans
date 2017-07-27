eb2c0e06dc432a5afd262dded2f8a10d28e97c40
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-27 20:45:15 +1000

    cleaned up file a bit

--- src/clj/kaidens_caravans/business_layer/cms.clj
+++ src/clj/kaidens_caravans/business_layer/cms.clj
@@ -7,7 +7,7 @@
             [clojure.string :as string]
             [clojure.string :as s]))
 
-(defn- s3-put
+(defn- s3-put!
   [stream bytes content-type file-name]
   (s3/put-object {:endpoint (env :aws-region)}
                  :bucket-name (env :bucket-name)
@@ -17,11 +17,12 @@
                              :content-type content-type}
                  :return-values "ALL_OLD"))
 
-(defn- upload-layout
+(defn- upload-layout!
   [template-name new-name params]
+  (clojure.tools.logging/debug "Uploading: " new-name)
   (let [bytes (.getBytes (.toString (kaidens-caravans.layout/render-html template-name params)) "UTF-8")
         stream (java.io.ByteArrayInputStream. bytes)]
-    (s3-put stream (count bytes) "text/html" new-name)))
+    (s3-put! stream (count bytes) "text/html" new-name)))
 
 (defn- file-content-type
   [file-name]
@@ -32,25 +33,34 @@
     "png" "image/png"
     ""))
 
-(defn- upload
+(defn- upload-file!
   [file file-path content-type]
   (clojure.tools.logging/debug "Uploading: " file-path)
   (let [s (io/input-stream file)
         b (.available s)]
-    (s3-put s b content-type file-path)))
+    (s3-put! s b content-type file-path)))
 
-(defn upload! []
+(defn- upload-static-files!
+  []
   (doseq [file (file-seq (io/file "./resources/public"))]
     (let [file-path (str (.getPath file))
           content-type (file-content-type file-path)]
       (if (not (empty? content-type))
-          (upload file (s/replace file-path #"\./resources/public/" "") content-type))))
+        (upload-file! file (s/replace file-path #"\./resources/public/" "") content-type)))))
+
+(defn- upload-dynamic-files!
+  []
   (let [caravans (filter #(not (:archived %)) (retrieve-caravans))]
-    (upload-layout "stock.html" "stock.html" {:stock (map (fn [m] (vector (first m) (second m))) (group-by :type caravans))})
+    (upload-layout! "stock.html" "stock.html" {:stock (map (fn [m] (vector (first m) (second m))) (group-by :type caravans))})
     (doseq [caravan caravans]
-      (upload-layout "singlestock.html" (str (:id caravan) ".html") caravan))
-    (upload-layout "caravan.html" "caravan.html" {})
-    (upload-layout "single_caravan.html" "single_caravan.html" {})
-    (upload-layout "homepage.html" "index.html" {}))
-  (response/ok "DONE"))
+      (upload-layout! "singlestock.html" (str (:id caravan) ".html") caravan))
+    (upload-layout! "caravan.html" "caravan.html" {})
+    (upload-layout! "single_caravan.html" "single_caravan.html" {})
+    (upload-layout! "homepage.html" "index.html" {})))
+
+(defn upload! []
+  (upload-static-files!)
+  (upload-dynamic-files!)
+  (clojure.tools.logging/debug "Upload Complete!")
+  (response/created (str "http://" (env :bucket-name) ".s3-website-ap-southeast-2.amazonaws.com")))
 
