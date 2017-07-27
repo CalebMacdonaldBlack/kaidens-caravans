13eddd233251de3cc27efae92a7a0d8dd6aac6da
Caleb MacDonald Black <calebmacdonaldblack@gmail.com>
2017-07-27 20:21:54 +1000

    now uploads whole directory

--- src/clj/kaidens_caravans/business_layer/cms.clj
+++ src/clj/kaidens_caravans/business_layer/cms.clj
@@ -4,7 +4,8 @@
             [amazonica.aws.s3 :as s3]
             [ring.util.http-response :as response]
             [kaidens-caravans.db.core :refer :all]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.string :as s]))
 
 (defn- s3-put
   [stream bytes content-type file-name]
@@ -16,39 +17,40 @@
                              :content-type content-type}
                  :return-values "ALL_OLD"))
 
-(defn- upload-page
-  [file-name content-type]
-  (let [s (io/input-stream (io/file (str "resources/public/" file-name)))
-        b (.available s)]
-    (s3-put s b content-type file-name)))
-
 (defn- upload-layout
   [template-name new-name params]
   (let [bytes (.getBytes (.toString (kaidens-caravans.layout/render-html template-name params)) "UTF-8")
         stream (java.io.ByteArrayInputStream. bytes)]
     (s3-put stream (count bytes) "text/html" new-name)))
 
-(def files
-  [["css/custom.css" "text/css"]
-   ["css/material.css" "text/css"]
-   ["css/website_custom.css" "text/css"]
-   ["js/material.js" "text/javascript"]
-   ["img/banner.jpg" "image/jpeg"]])
+(defn- file-content-type
+  [file-name]
+  (case (last (s/split file-name #"\."))
+    "css" "text/css"
+    "js" "text/javascript"
+    "jpg" "image/jpeg"
+    "png" "image/png"
+    ""))
+
+(defn- upload
+  [file file-path content-type]
+  (clojure.tools.logging/debug "Uploading: " file-path)
+  (let [s (io/input-stream file)
+        b (.available s)]
+    (s3-put s b content-type file-path)))
 
 (defn upload! []
-  (doseq [file files]
-    (upload-page (first file) (second file)))
-  (let [caravans (retrieve-caravans)]
+  (doseq [file (file-seq (io/file "./resources/public"))]
+    (let [file-path (str (.getPath file))
+          content-type (file-content-type file-path)]
+      (if (not (empty? content-type))
+          (upload file (s/replace file-path #"\./resources/public/" "") content-type))))
+  (let [caravans (filter #(not (:archived %)) (retrieve-caravans))]
     (upload-layout "stock.html" "stock.html" {:stock (map (fn [m] (vector (first m) (second m))) (group-by :type caravans))})
     (doseq [caravan caravans]
-      (upload-layout "singlestock.html" (str (:id caravan) ".html") caravan)))
-
-  (upload-layout "caravan.html" "caravan.html" {})
-  (upload-layout "single_caravan.html" "single_caravan.html" {})
-  (upload-layout "homepage.html" "index.html" {})
+      (upload-layout "singlestock.html" (str (:id caravan) ".html") caravan))
+    (upload-layout "caravan.html" "caravan.html" {})
+    (upload-layout "single_caravan.html" "single_caravan.html" {})
+    (upload-layout "homepage.html" "index.html" {}))
   (response/ok "DONE"))
-  ;(upload-page (slurp "resources/public/css/custom.css") "css/custom.css" "text/css")
-  ;(upload-page (slurp "resources/public/css/material.css") "css/material.css" "text/css")
-  ;(upload-page (slurp "resources/public/css/website_custom.css") "css/website_custom.css" "text/css")
-  ;(response/ok (upload-page (.toString (kaidens-caravans.layout/render-html "homepage.html")) "index.html" "text/html")))
 
